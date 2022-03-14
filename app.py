72e855c9bc6d876e851100e6d0439eeb5fe44826
18038 <18038@wc.school.nz>
2022-03-15 11:13:16 +1300

    added menu information using loop function and connecting to the data base

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
+import sqlite3
+from sqlite3 import Error
+
+DB_NAME = "smile.db"
 
 app = Flask(__name__)
+
+def create_connection(db_file):
+    """create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        # iniilalise_table(connection)
+        return connection
+    except Error as e:
+        print(e)
+
+        return None
+
 
 
 @app.route('/')
 def render_homepage():
-    #return '<h1>Hello, World!</h1>'
     return render_template("home.html")
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template("menu.html")
+
+
+    # connect to the database
+    con = create_connection(DB_NAME)
+
+    # SELECT the things you want from your table(s)
+    query = "SELECT name, description, volume, price, image FROM product"
+
+    cur = con.cursor() # You need this line next
+    cur.execute(query) # this line will actually execute the query
+    product_list = cur.fetchall() # puts the results into a list usable in python
+    con.close()
+
+    return render_template("menu.html", products=product_list)
 
 @app.route('/contact')
 def render_contact():
